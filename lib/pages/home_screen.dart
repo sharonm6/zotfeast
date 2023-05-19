3d2766c52e997b3afdffb2130bba24bc38cdbb4e
sharonm6 <sharon.m0626@gmail.com>
2023-05-19 01:07:30 -0700

    Configure constants for text and color

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -10,6 +10,14 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          "TESTINGGGG",
+          style: Theme.of(context).textTheme.bodyLarge,
+        ),
+      ),
+    );
   }
 }
