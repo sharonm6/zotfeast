3817704a2d111604dba7d38f340c07d3fedb29a3
sharonm6 <sharon.m0626@gmail.com>
2023-05-18 17:39:09 -0700

    File and route setup

---
+++ lib/pages/recipe_list_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RecipeListScreen extends StatefulWidget {
+  const RecipeListScreen({super.key});
+
+  @override
+  State<RecipeListScreen> createState() => _RecipeListScreenState();
+}
+
+class _RecipeListScreenState extends State<RecipeListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/pages/recipe_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RecipeScreen extends StatefulWidget {
+  const RecipeScreen({super.key});
+
+  @override
+  State<RecipeScreen> createState() => _RecipeScreenState();
+}
+
+class _RecipeScreenState extends State<RecipeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
