562ea5300567d951c5bbfe510fd76ab3712723a6
bdogetlauncher <bdogetlauncher@gmail.com>
2023-06-09 17:35:13 -0700

    Changed Model to num Events

--- python_ml/data/csv_creater2.py
+++ python_ml/data/csv_creater2.py
@@ -2,19 +2,8 @@ import os
 import pandas as pd
 import random
 
-def create_fake_schedule():
-    # create a list of random 0 or 1 values with 32 elements
-    # 0 = no class, 1 = class
-    schedule = [random.randint(0, 1) for i in range(32)]
-    while sum(schedule) > 16:
-        schedule = [random.randint(0, 1) for i in range(32)]
-    return schedule
-
-def conv_schedule_to_str(schedule):
-    strSch = ""
-    for i in range(32):
-        strSch += str(schedule[i])
-    return strSch
+def create_fake_availaibilities(): # number of events within next 2.5 hrs
+    return random.randint(0,5)
 
 def create_fake_dist():
     # return a random number between 0 and 10 float
@@ -29,30 +18,27 @@ def create_fake_duration():
 def create_fake_time():
     return random.randint(0, 31)
 
-def generate_response(schedule, time, dist, task, duration):
-    duration /= .5
+def generate_response(nEvents, dist, task, duration):
+    duration /= 2
     dist_time = dist/2 # dist_time = dist * 15 min * 2 = dist * 30 min = dist
     
     if dist > 4:
         return 0
-    if schedule[time] == '1':
-        return 0
-    elif '1' in schedule[time : int(time + duration + dist_time - 0.5)]:
-        return 0
-    if time > 28 or time < 4:
+    if nEvents == 0:
+        return 1
+    if nEvents + dist_time + duration > 5:
         return 0
     return 1
 
 def create_fake_data(num):
-    df = pd.DataFrame(columns=['Schedule', 'Time', 'Distance', 'Task', 'Duration', 'Response'])
+    df = pd.DataFrame(columns=['NumEvents', 'Distance', 'Task', 'Duration', 'Response'])
     for i in range(num):
-        schedule = create_fake_schedule()
-        time = create_fake_time()
+        nEvents = create_fake_availaibilities()
         dist = create_fake_dist()
         task = create_fake_task()
         duration = create_fake_duration()
-        response = generate_response(schedule, time, dist, task, duration)
-        df.loc[i] = [conv_schedule_to_str(schedule), time, dist, task, duration, response]
+        response = generate_response(nEvents, dist, task, duration)
+        df.loc[i] = [nEvents, dist, task, duration, response]
     return df
 
 def save_data(df, filename, dir='input'):
